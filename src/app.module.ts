2a7e866b3be0ca3e5cf3e5cfaa0530c52b79fdf5
caiodonat <cdonat.dev@gmail.com>
2023-11-10 21:33:14 +0000

    try: test x5

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [ConfigModule, DatabaseModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule { }
