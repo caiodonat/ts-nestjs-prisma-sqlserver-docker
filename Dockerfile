699dae4aaba228dca81d6e54c4ef552f4923b061
cdonat-ist <cdonat@findes.org.br>
2024-04-18 16:31:35 -0300

    build: docker test 14

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
+# /Dockerfile (raiz do projeto)
+
+# Sempre utilizar a variante `alpine`.
 FROM node:20-alpine3.18
 
-WORKDIR /app
+# Boa pratica separa o *código fonte* dos demais arquivos do sistema.
+# WORKDIR /app
+WORKDIR /
 
 COPY . .
 
+# Garantindo que nosso script de inicialização tenha as permições necessárias para
+#  funcionar corretamente.
+RUN chmod +x docker_entrypoint.sh
 
-RUN npm i
-# Prisma Setup
-
-RUN chmod +x entrypoint.sh
-
-ENTRYPOINT ["sh", "entrypoint.sh"]
+# Nas variantes `alpine` é necessário utiliza `sh` para executar arquivos de scripts (.sh)
+ENTRYPOINT ["sh", "docker_entrypoint.sh"]
---
+++ docker_entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# /docker_entrypoint.sh (raiz do projeto)
+
+# Como a imagem Docker que estamos utilizando (node:20-alpine3.18) já vem com o
+#  `NPM` instalado, podemos pular a etapa de instalação do mesmo.
+npm i
+
+# No caso do `Prisma`, para que o *build* sejá bem sucedido é necessário ter a 'tipagem' das entidades
+#  do banco de dados (schema.prisma)
+npx prisma generate --schema=./src/database/prisma/schema.prisma
+
+npm run build
+
+# Setup do ORM (Prisma)
+
+# # if $some_parameter run `npx prisma migrate reset`
+npx prisma migrate reset
+# npx prisma migrate deploy
+
+# CLI final para executar a aplicação
+exec npm run start
